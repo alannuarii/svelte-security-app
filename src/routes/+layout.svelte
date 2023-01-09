192f07fb3978029b20a1fffbc4515f0b0b4a8ec2
alannuarii <alannuariii@gmail.com>
2023-01-09 17:44:36 +0800

    Update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import './styles.css';
+	import Header from './Header.svelte';
+	import Menu from './Menu.svelte';
+	import Bottom from './Bottom.svelte';
+</script>
+
+<div>
+	<Header />
+	<main>
+		<Menu />
+	</main>
+	<Bottom />
+</div>
+
+<style>
+</style>
---
+++ src/routes/Menu.svelte
@@ -0,0 +1,60 @@
+<script>
+	import guestbook from '../lib/img/guestbook.png';
+	import patrol from '../lib/img/patrol.png';
+	import apel from '../lib/img/apel.png';
+	import cctv from '../lib/img/cctv.png';
+
+	const icons = [
+		{
+			img: guestbook,
+			name: 'Buku Tamu'
+		},
+		{
+			img: patrol,
+			name: 'Patroli'
+		},
+		{
+			img: apel,
+			name: 'Apel'
+		},
+		{
+			img: cctv,
+			name: 'CCTV'
+		}
+	];
+</script>
+
+<section id="menu">
+	<div class="container text-center p-3">
+		<div class="row row-cols-2 gy-4">
+			{#each icons as icon}
+				<div class="col flex-column d-flex align-items-center">
+					<div class="icon-box border shadow">
+						<img src={icon.img} class="img-fluid" alt="" />
+					</div>
+					<h6>{icon.name}</h6>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
+
+<style>
+	#menu {
+		overflow: hidden;
+	}
+	#menu .icon-box {
+		padding: 30px 20px;
+		transition: ease-in-out 0.3s;
+		/* border: 1px solid rgba(255, 255, 255, 0.3); */
+		/* height: 100%; */
+		width: 150px;
+		text-align: center;
+        border-radius: 20px 50px;
+	}
+	#menu h6 {
+		font-weight: 300;
+		font-size: 20px;
+		margin-top: 15px;
+	}
+</style>
