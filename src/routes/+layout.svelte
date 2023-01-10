dd4d086f875c30999308b5f7d7bd1ed9ef75f208
alannuarii <alannuariii@gmail.com>
2023-01-10 08:27:41 +0800

    Update komponen info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,16 @@
 <script>
 	import './styles.css';
 	import Header from './Header.svelte';
-	import Menu from './Menu.svelte';
 	import Bottom from './Bottom.svelte';
 </script>
 
 <div>
 	<Header />
 	<main>
-		<Menu />
+		<slot />
 	</main>
 	<Bottom />
 </div>
 
 <style>
-	
 </style>
---
+++ src/routes/Info.svelte
@@ -0,0 +1,5 @@
+<section>
+	<div class="alert alert-dark mx-2 mt-2" role="alert">
+		Patroli telah dilaksanakan oleh <strong>Imran Mokodompit</strong> pada pukul 08.00 WITA
+	</div>
+</section>
