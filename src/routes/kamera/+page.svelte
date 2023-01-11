0426b22d462eaa6ca21e8d5a8d6a8fb4e2dd5355
alannuarii <alannuariii@gmail.com>
2023-01-11 15:50:39 +0800

    Update

--- src/routes/kamera/+page.svelte
+++ src/routes/kamera/+page.svelte
@@ -1,50 +1,41 @@
 <script>
-	let video;
-	let canvas;
-	let context;
+	let constraints = { video: { facingMode: 'environment' } };
 	let stream;
-	let constraints = {
-		video: { facingMode: 'environment' },
-		audio: false
-	};
+	let mediaRecorder;
 
-	function successCallback(stream) {
+	function handleSuccess(stream) {
 		video.srcObject = stream;
+		mediaRecorder = new MediaRecorder(stream);
 	}
 
-	function errorCallback(error) {
+	function handleError(error) {
 		console.log('navigator.getUserMedia error: ', error);
 	}
 
-	function start() {
-		navigator.mediaDevices.getUserMedia(constraints).then(successCallback).catch(errorCallback);
+	function takeSnapshot() {
+		let canvas = document.createElement('canvas');
+		canvas.width = video.videoWidth;
+		canvas.height = video.videoHeight;
+		canvas.getContext('2d').drawImage(video, 0, 0);
+		// you can now save the image from the canvas
 	}
 
-	function switchCamera() {
-		constraints.video.facingMode =
-			constraints.video.facingMode == 'environment' ? 'user' : 'environment';
-		start();
+	function startRecording() {
+		mediaRecorder.start();
 	}
 
-	function snapshot() {
-		context.drawImage(video, 0, 0, canvas.width, canvas.height);
+	function stopRecording() {
+		mediaRecorder.stop();
 	}
 
-	function stop() {
-		if (stream) {
-			stream.getTracks().forEach(function (track) {
-				track.stop();
-			});
-		}
-	}
+	navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
 </script>
 
 <section id="camera" class="p-3">
-	<button on:click={switchCamera}>switch camera</button>
-	<button on:click={snapshot}>take snapshot</button>
-	<button on:click={stop}>stop camera</button>
-	<button on:click={start}>start camera</button>
 	<!-- svelte-ignore a11y-media-has-caption -->
-	<video id="video" autoplay playsinline ref={video} />
-	<canvas id="canvas" ref={canvas} />
+	<video id="video" autoplay playsinline bing:this={stream} />
+
+	<button on:click={takeSnapshot}>Take snapshot</button>
+	<button on:click={startRecording}>Start Recording</button>
+	<button on:click={stopRecording}>Stop Recording</button>
 </section>
