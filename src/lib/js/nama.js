d43a24bb3e6d0b542b759e86e61cf68dc5aaea3a
alannuarii <alannuariii@gmail.com>
2023-02-01 11:46:40 +0800

    Update

---
+++ src/lib/js/nama.js
@@ -0,0 +1,15 @@
+export function namaSecurity(nama) {
+	if (nama === 'imran') {
+		return 'Imran Mokodompit';
+	} else if (nama === 'faisal') {
+		return 'Faisal Manopode';
+	} else if (nama === 'karter') {
+		return 'Karter Tanduk';
+	} else if (nama === 'jerry') {
+		return 'Jerry Legoh';
+	} else if (nama === 'ewin') {
+		return 'Michael Baranoy';
+	} else if (nama === 'jonly') {
+		return 'Jonly Tulandi';
+	}
+}
--- src/routes/jadwal/+page.svelte
+++ src/routes/jadwal/+page.svelte
@@ -1,44 +1,69 @@
 <script>
 	import TitleDate from '../components/TitleDate.svelte';
 	import { jadwalPiket } from '../../lib/js/jadwal';
+	import { namaSecurity } from '../../lib/js/nama';
+	import { onMount } from 'svelte';
 
 	let tanggal = new Date().toISOString().slice(0, 10);
+	let pagi = [];
+	let sore = [];
+	let malam = [];
+	let jadwal = jadwalPiket(tanggal);
 
 	function handleEvent(event) {
 		tanggal = event.detail;
+		pagi = [];
+		sore = [];
+		malam = [];
+		jadwal = jadwalPiket(tanggal);
+		getName();
 	}
 
-	$: console.log(jadwalPiket(tanggal));
+	function getName() {
+		Object.entries(jadwal).forEach(([key, value]) => {
+			if (value === 'p') {
+				pagi.push(key);
+				pagi = [...pagi];
+			} else if (value === 's') {
+				sore.push(key);
+				sore = [...sore];
+			} else if (value === 'm') {
+				malam.push(key);
+				malam = [...malam];
+			}
+		});
+	}
+
+	onMount(() => {
+		getName();
+	});
 </script>
 
 <section class="p-3">
 	<TitleDate title={'jadwal piket'} on:tanggal={handleEvent} />
 	<div class="border p-3 text-center mb-2 shadow-sm">
 		<h5 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h5>
-		<div class="p-3 mb-2 bg-dark-subtle">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
-		<div class="bg-dark-subtle p-3">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
+		{#each malam as m}
+			<div class="p-3 mb-2 bg-body-secondary">
+				<h6><i class="bi-person-circle me-2" /> {namaSecurity(m)}</h6>
+			</div>
+		{/each}
 	</div>
 	<div class="border p-3 text-center mb-2 shadow-sm">
-		<h5 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h5>
-		<div class="bg-dark-subtle p-3 mb-2">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
-		<div class="bg-dark-subtle p-3">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
+		<h5 class="mb-3"><i class="bi-sun me-2" /> Piket Pagi</h5>
+		{#each pagi as p}
+			<div class="p-3 mb-2 bg-body-secondary">
+				<h6><i class="bi-person-circle me-2" /> {namaSecurity(p)}</h6>
+			</div>
+		{/each}
 	</div>
 	<div class="border p-3 text-center mb-2 shadow-sm">
-		<h5 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h5>
-		<div class="bg-dark-subtle p-3 mb-2">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
-		<div class="bg-dark-subtle p-3">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
+		<h5 class="mb-3"><i class="bi-sunset me-2" /> Piket Sore</h5>
+		{#each sore as s}
+			<div class="p-3 mb-2 bg-body-secondary">
+				<h6><i class="bi-person-circle me-2" /> {namaSecurity(s)}</h6>
+			</div>
+		{/each}
 	</div>
 </section>
 
