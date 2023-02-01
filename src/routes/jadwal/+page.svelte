509c65029883b01b1e186073fc22545cfeabf24d
alannuarii <alannuariii@gmail.com>
2023-02-01 09:46:53 +0800

    Update

--- src/routes/jadwal/+page.svelte
+++ src/routes/jadwal/+page.svelte
@@ -15,28 +15,28 @@
 	<TitleDate title={'jadwal piket'} on:tanggal={handleEvent} />
 	<div class="border p-3 text-center mb-2">
 		<h6 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h6>
-		<div class="border p-2 mb-2">
+		<div class="border p-3 mb-2">
 			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
 		</div>
-		<div class="border p-2">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
-	</div>
-	<div class="border p-3 text-center mb-2">
-		<h6 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h6>
-		<div class="border p-2 mb-2">
-			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
-		</div>
-		<div class="border p-2">
+		<div class="border p-3">
 			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
 		</div>
 	</div>
 	<div class="border p-3 text-center mb-2">
 		<h6 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h6>
-		<div class="border p-2 mb-2">
+		<div class="border p-3 mb-2">
 			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
 		</div>
-		<div class="border p-2">
+		<div class="border p-3">
+			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
+		</div>
+	</div>
+	<div class="border p-3 text-center mb-2">
+		<h6 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h6>
+		<div class="border p-3 mb-2">
+			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
+		</div>
+		<div class="border p-3">
 			<h6><i class="bi-person-circle me-2" /> Imran Mokodompit</h6>
 		</div>
 	</div>
