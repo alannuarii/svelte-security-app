64126308dec01bd2c6397d7bb535b511cc2a258c
alannuarii <alannuariii@gmail.com>
2023-02-01 11:52:51 +0800

    Update

--- src/routes/jadwal/+page.svelte
+++ src/routes/jadwal/+page.svelte
@@ -42,7 +42,8 @@
 <section class="p-3">
 	<TitleDate title={'jadwal piket'} on:tanggal={handleEvent} />
 	<div class="border p-3 text-center mb-2 shadow-sm">
-		<h5 class="mb-3"><i class="bi-moon-stars me-2" /> Piket Malam</h5>
+		<h5><i class="bi-moon-stars me-2" /> Piket Malam</h5>
+		<p>(Pukul 24:00 - 08:00 WITA)</p>
 		{#each malam as m}
 			<div class="p-3 mb-2 bg-body-secondary">
 				<h6><i class="bi-person-circle me-2" /> {namaSecurity(m)}</h6>
@@ -50,7 +51,8 @@
 		{/each}
 	</div>
 	<div class="border p-3 text-center mb-2 shadow-sm">
-		<h5 class="mb-3"><i class="bi-sun me-2" /> Piket Pagi</h5>
+		<h5><i class="bi-moon-stars me-2" /> Piket Pagi</h5>
+		<p>(Pukul 08:00 - 16:00 WITA)</p>
 		{#each pagi as p}
 			<div class="p-3 mb-2 bg-body-secondary">
 				<h6><i class="bi-person-circle me-2" /> {namaSecurity(p)}</h6>
@@ -58,7 +60,8 @@
 		{/each}
 	</div>
 	<div class="border p-3 text-center mb-2 shadow-sm">
-		<h5 class="mb-3"><i class="bi-sunset me-2" /> Piket Sore</h5>
+		<h5><i class="bi-moon-stars me-2" /> Piket Sore</h5>
+		<p>(Pukul 16:00 - 24:00 WITA)</p>
 		{#each sore as s}
 			<div class="p-3 mb-2 bg-body-secondary">
 				<h6><i class="bi-person-circle me-2" /> {namaSecurity(s)}</h6>
@@ -75,5 +78,10 @@
 	h6 {
 		font-size: 12px;
 		margin-bottom: -1px;
+	}
+	p {
+		font-size: 10px;
+		margin-top: -3px;
+		margin-bottom: 10px;
 	}
 </style>
